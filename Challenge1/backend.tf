4ea4308c807fd4f4a683b85513600edc236968cb
nilabja-OpenSC <44666518+nilabja-OpenSC@users.noreply.github.com>
2022-04-29 02:51:43 +0530

    Add files via upload

--- Challenge1/backend.tf
+++ Challenge1/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "tf-state-bucket"
+        bucket = "tf-state-bucket-challenge"
         key    = "development/terraform_state"
         region = "us-east-2"
     }
