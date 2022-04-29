db25d1b45988a01f911d6c5621ec15816d59a37e
nilabja-OpenSC <44666518+nilabja-OpenSC@users.noreply.github.com>
2022-04-29 23:55:05 +0530

    Update webserver_instance.tf

--- Challenge1/webserver/webserver_instance.tf
+++ Challenge1/webserver/webserver_instance.tf
@@ -100,7 +100,7 @@ resource "aws_autoscaling_group" "project_webserver" {
   max_size                  = 2
   min_size                  = 1
   health_check_grace_period = 30
-  health_check_type         = "EC2"
+  health_check_type         = "ELB"
   desired_capacity          = 1
   force_delete              = true
   launch_template {
